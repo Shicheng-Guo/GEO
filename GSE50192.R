a93dd5c2465c87957dcf69fa625c97fbf6769f8b
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-07-28 13:07:42 -0700

    GEO

---
+++ GSE50192.R
@@ -0,0 +1,58 @@
+# For PCA Analysis to methylation 450K dataset
+# for ips methylatin 450K analysis
+# setwd("G:\\geo")
+library("GEOquery")
+setwd("/home/shg047/oasis/GEO")
+
+GSEID<-"GSE50192"
+GEO <- getGEO(GSEID,destdir="/home/shg047/oasis/GEO")
+library("GEOquery")
+save(GEO,file=paste(GSEID,".RData",sep=""))
+load(paste(GSEID,".RData",sep=""))
+data <- as.data.frame(exprs(GEO[[1]]))
+phen <- pData(phenoData(GEO[[1]]))
+
+phen1<-sapply(strsplit(as.character(phen$characteristics_ch1.1),": "),function(x) unlist(x)[2])  # status
+phen1[phen1=="rheumatoid arthritis"]<-"Rheumatoid Arthritis"
+phen2<-sapply(strsplit(as.character(phen$characteristics_ch1.3),": "),function(x) unlist(x)[2])  # age
+phen3<-sapply(strsplit(as.character(phen$characteristics_ch1.4),": "),function(x) unlist(x)[2])  # gender
+phen3[phen3=="f"]<-"Female"
+phen3[phen3=="m"]<-"Male"
+
+
+PCAPlot<-function(data,pheno,output,multifigure=T){
+  pca <- prcomp(data,center=T,scale = F)  # Here, input file: row is individual and column is variable
+  outputfile=paste(output,".pdf",sep="")
+  pdf(outputfile)
+  if(multifigure){
+    par(mfrow=c(2,2),mar=c(4,4,4,4)) 
+  }
+  plot((pca$sdev[1:10])^2,type="o",xaxt="n",ylab="Variances",xlab="Principle Components",col="red",lwd=2)
+  axis(1,at=0:10,labels=paste("PC",0:10,sep=""))
+  var<-c()
+  for(i in 1:length(pca$sdev)){var[i]<-sum((pca$sdev[1:i])^2)/sum((pca$sdev)^2)}
+  plot(var,ylab="total variance",xlab="number of principle components",lwd=2,type="l")
+  abline(h=0.8,col="grey",lty=2)
+  abline(v=which(var>0.8)[1],col="grey",lty=2)
+  scores <- data.frame(pheno, pca$x[,1:3])
+  col = as.numeric(as.factor(pheno))
+  plot(x=scores$PC1,y=scores$PC2, xlim=c(min(scores$PC1),max(scores$PC1)),ylim=c(min(scores$PC2),max(scores$PC2)),type="n",xlab="PC1",ylab="PC2")
+  for(i in 1:length(scores$PC1)){
+    points(scores$PC1[i],scores$PC2[i],pch=as.numeric(as.factor(pheno))[i],col=col[i],cex=0.8,lwd=2)
+  }
+  legend("topleft",legend=names(table(pheno)),pch=1:length(table(pheno)),col=1:length(table(pheno)),bty="n",pt.lwd=2)
+  plot(x=scores$PC1,y=scores$PC3, xlim=c(min(scores$PC1),max(scores$PC1)),ylim=c(min(scores$PC3),max(scores$PC3)),type="n",xlab="PC1",ylab="PC3")
+  for(i in 1:length(scores$PC1)){
+    points(scores$PC1[i],scores$PC3[i],pch=as.numeric(as.factor(pheno))[i],col=col[i],cex=0.9,lwd=2)
+  }
+  legend("bottomleft",legend=names(table(pheno)),pch=1:length(table(pheno)),col=1:length(table(pheno)),bty="n",pt.lwd=2)
+  dev.off()
+}
+
+data1=na.omit(data)
+PCAPlot(t(data1),phen1,output="GSE42861_status.pca.pdf",multifigure=T)
+PCAPlot(t(data1),phen3,output="GSE42861_gender.pca.pdf",multifigure=T)
+PCAPlot(t(data1),phen2,output="GSE42861_age.pca.pdf",multifigure=T)
+
+
+
