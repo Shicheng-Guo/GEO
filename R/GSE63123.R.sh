9ec21ff7fc6bac4a0986c78db4dd1bf492c35b45
Shicheng Guo <Shicheng.Guo@hotmail.com>
2017-01-25 14:55:16 -0800

    Update GSE63123.R.sh

--- R/GSE63123.R.sh
+++ R/GSE63123.R.sh
@@ -6,5 +6,12 @@
 
 /media/Home_Raid1/dinh/NAS3_volume1_mnt/shg047/GEO/GSE63123_RAW.tar
 tar xvf GSE63123_RAW.tar
+gunzip *.gz
+rm GSE63123_RAW.tar
+head GSM2330155_A124PrF.cg.txt
+
+# format: chr     position        strand  methylated_read_count   total_read_count
+
+
 
 
