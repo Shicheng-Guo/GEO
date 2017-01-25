9ea05943ba45322ac4125db14651aa38a88af63e
Shicheng Guo <Shicheng.Guo@hotmail.com>
2017-01-25 14:45:47 -0800

    Update GSE63123.R.sh

--- R/GSE63123.R.sh
+++ R/GSE63123.R.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/sh
 
+# Large-scale epigenetic reprogramming is punctuated late during the evolution of pancreatic cancer progression
+# https://www.ncbi.nlm.nih.gov/pubmed/28092686
+# https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE63123
+
 /media/Home_Raid1/dinh/NAS3_volume1_mnt/shg047/GEO/GSE63123_RAW.tar
 tar xvf GSE63123_RAW.tar
 
