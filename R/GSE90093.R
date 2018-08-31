538796c951738e98f3783510974d7e00804c0fae
Shicheng Guo <Shicheng.Guo@hotmail.com>
2018-08-31 14:28:50 -0500

    Update GSE90093.R

--- R/GSE90093.R
+++ R/GSE90093.R
@@ -1,1 +1,2 @@
 # Genomic DNA from dissected Alb-R26Met tumors (n=10) and control livers (n=3) was used to analyze the DNA methylation profile
+# https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE90093
