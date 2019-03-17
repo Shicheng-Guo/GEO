f8f077d000bd7a2e6c28c1b0a73388718203da2d
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-03-17 17:19:22 -0500

    Update GSE76269.R

--- R/GSE76269.R
+++ R/GSE76269.R
@@ -1,5 +1,4 @@
-```
+
 system("wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE76nnn/GSE76269/suppl/GSE76269_betas.extra.epigenomics.tsv.gz")
 system("gunzip GSE76269_betas.extra.epigenomics.tsv.gz")
 data<-read.table("~/hpc/methylation/GEO/GSE76269_betas.extra.epigenomics.tsv",head=T,row.names=1,sep="\t")
-```
